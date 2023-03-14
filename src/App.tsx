40516d51ff294136a03e0d372486b6e2e8669288
stassb129 <stassb130@mail.ru>
2023-03-14 16:45:47 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Card from "./components/Card/Card";
 function App() {
     const searchLinks = [
         {
-            name: "googl",
+            name: "google",
             link: "https://www.google.com/"
         },
         {
